6635c2be82bfa64f8d59b4f369470e7e87d117a7
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-02-02 09:14:52 -0300

    Exception ResourceNotFound

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -29,7 +29,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
--- src/main/java/com/ecommerce/project/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.handler;
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -22,5 +23,11 @@ public class GlobalExceptionHandler {
         });
         return new ResponseEntity<Map<String, String>>(response,
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 }
