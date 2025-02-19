651d4e07edbdd8f510c99c7f52827e47e58cc995
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-02-19 09:34:27 -0300

    relationships cart

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -47,6 +47,10 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "address_id"))
     private List<Address> addresses = new ArrayList<>();
 
+    @ToString.Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
+    private Cart cart;
+
     public User(String userName, String email, String password) {
         this.userName = userName;
         this.email = email;
